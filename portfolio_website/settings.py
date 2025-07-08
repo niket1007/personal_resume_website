996a84156a8e2c3819c3b7de56a20f8106833b3b
Niket Jain <niket.jain@accolitedigital.com>
2025-07-09 03:19:50 +0530

    vercel required changes

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,8 +105,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'prod_static'
+STATIC_ROOT = BASE_DIR / 'staticfiles_prod'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
